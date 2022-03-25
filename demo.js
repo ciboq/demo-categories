30d6a0c7ee59cc8945259175b0d5966c7863b95f
ciboq <90256806+ciboq@users.noreply.github.com>
2022-03-25 11:08:20 -0400

    m

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
+console.log('hi')
 
-fetch('https://api.airtable.com/v0/appAcEvq8fb5jWc6B/Table%201')
+fetch('https://api.airtable.com/v0/appAcEvq8fb5jWc6B/albums', {
+    headers: {
+        Authorization: 'Bearer keyU0oov6exskr2Q0',
+    },
+})
 .then(response => response.json())
 .then(data => {
     console.log(data);
