8687cf7099e270003657c941877a858c500da8de
ciboq <90256806+ciboq@users.noreply.github.com>
2022-04-03 23:13:26 -0400

    m

--- demo.js
+++ demo.js
@@ -8,4 +8,16 @@ fetch('https://api.airtable.com/v0/appAcEvq8fb5jWc6B/albums', {
 .then(response => response.json())
 .then(data => {
     console.log(data);
+    const container = document.querySelector('.container');
+    data.records.forEach(album => {
+    console.log(album);
+
+
+container.innerHTML += `
+<div class="album">
+<h3>${album.fields.file}</h3>
+
+</div>
+`;
+});
 });
